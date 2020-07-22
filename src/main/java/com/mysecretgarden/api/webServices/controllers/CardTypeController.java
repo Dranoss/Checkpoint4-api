3091b51ecc08ec1ff31880e93d77b926c11d2da9
Xavi <xaviregnier@gmail.com>
2020-07-22 20:51:16 +0200

    crud: CardType and User controllers done

--- src/main/java/com/mysecretgarden/api/webServices/controllers/CardTypeController.java
+++ src/main/java/com/mysecretgarden/api/webServices/controllers/CardTypeController.java
@@ -1,4 +1,39 @@
 package com.mysecretgarden.api.webServices.controllers;
 
+import com.mysecretgarden.api.webServices.entities.CardType;
+import com.mysecretgarden.api.webServices.services.CardTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class CardTypeController {
+
+    @Autowired
+    private CardTypeService cardTypeService;
+
+    @GetMapping
+    public List<CardType> getCards(){
+        return cardTypeService.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public CardType getCardById(@PathVariable Long id){
+        return cardTypeService.getCardTypeById(id);
+    }
+
+    @PostMapping
+    public CardType postCard(@RequestBody CardType cardType){
+        return cardTypeService.save(cardType);
+    }
+
+    @PutMapping(value = "/{id}")
+    public CardType putCard(@RequestBody CardType cardType, @PathVariable Long id){
+        return cardTypeService.update(cardType, id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteCard(@PathVariable Long id){
+        cardTypeService.deleteCardType(id);
+    }
 }
--- src/main/java/com/mysecretgarden/api/webServices/controllers/UserController.java
+++ src/main/java/com/mysecretgarden/api/webServices/controllers/UserController.java
@@ -1,4 +1,41 @@
 package com.mysecretgarden.api.webServices.controllers;
 
+import com.mysecretgarden.api.webServices.entities.User;
+import com.mysecretgarden.api.webServices.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getCards(){
+        return userService.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public User getCardById(@PathVariable Long id){
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public User postCard(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+
+    @PutMapping(value = "/{id}")
+    public User putCard(@RequestBody User user, @PathVariable Long id){
+        return userService.updateUser(user, id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteCard(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
 }
